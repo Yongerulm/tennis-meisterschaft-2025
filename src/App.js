1fac6cb899c857e870a6330d001cd961a826c417
Markus <96618226+Yongerulm@users.noreply.github.com>
2025-05-30 09:32:34 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TennisChampionship from './components/TennisChampionship';
+import './styles/index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TennisChampionship />
+    </div>
+  );
+}
+
+export default App;
